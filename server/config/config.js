bc55778937c68e044e3905c06378c9c1862f9c4a
camilablnpa <camilapinto@outlook.cl>
2020-09-28 19:07:28 -0300

    Variable de entorno de heroku

--- server/config/config.js
+++ server/config/config.js
@@ -7,7 +7,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://camilablnpa:s5hQlhUJYUIXuHJV@cluster0.si3zo.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URL_DB = urlDB;
